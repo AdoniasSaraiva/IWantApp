077ba062e87c080c966b35ad903997bba86645c0
adonias das neves saraiva <adonias2022@gmail.com>
2023-01-17 21:37:56 -0300

    COMMIT - Adicionando nova dependencia para fazer validações e refatorando end-point de criação

--- Domain/Products/Category.cs
+++ Domain/Products/Category.cs
@@ -1,7 +1,26 @@
-﻿namespace IWantApp.Domain.Products;
+﻿using Flunt.Validations;
+using IWantApp.Utils;
+namespace IWantApp.Domain.Products;
 
 public class Category : Entity
 {
     public string Name { get; set; }
-    public bool Active { get; set; } = true;
+    public bool Active { get; set; }
+
+    public Category(string name, string createdBy, string editedBy)
+    {
+        var contract = new Contract<Category>()
+            .IsNotNullOrEmpty(name, "Name", ServiceMsg.MSG0001)
+            .IsNotNullOrEmpty(CreatedBy, "CreatedBy", ServiceMsg.MSG0002)
+            .IsNotNullOrEmpty(EditedBy, "EditedBy", ServiceMsg.MSG0003);
+
+
+        AddNotifications(contract);
+        Name = name;
+        Active = true;
+        CreatedBy = createdBy;
+        EditedBy = editedBy;
+        CreatedOn = DateTime.Now;
+        EditedOn = DateTime.Now;
+    }
 }
